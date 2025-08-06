010d5f4dce3a5d696af72e2348be18c299ce672d
John J <76699031+john310897@users.noreply.github.com>
2025-08-06 08:23:55 +0000

    intial commit and intial design updated

--- backend/main.py
+++ backend/main.py
@@ -2,24 +2,45 @@ import os
 from azure.ai.inference import ChatCompletionsClient
 from azure.ai.inference.models import SystemMessage, UserMessage
 from azure.core.credentials import AzureKeyCredential
+from flask import Flask
+from flask_cors import CORS
 
-endpoint = "https://models.github.ai/inference"
-model = "openai/gpt-4.1"
-token = os.environ["GITHUB_TOKEN"]
 
-client = ChatCompletionsClient(
-    endpoint=endpoint,
-    credential=AzureKeyCredential(token),
-)
+app = Flask(__name__)
+CORS(app,supports_credentials=True)
 
-response = client.complete(
-    messages=[
-        SystemMessage("You are a helpful assistant."),
-        UserMessage("What is the capital of France?"),
-    ],
-    temperature=1.0,
-    top_p=1.0,
-    model=model
-)
 
-print(response.choices[0].message.content)
+@app.route('/')
+def backend_check():
+    return "Backend works"
+
+@app.route('/query/<search_stream>')
+def search(search_stream):
+    output={'client':False,'message':"hello how can i help you"}
+    # print(output)
+    # return output
+    endpoint = "https://models.github.ai/inference"
+    model = "openai/gpt-4.1"
+    token = os.environ["GITHUB_TOKEN"]
+
+    client = ChatCompletionsClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(token),
+    )
+
+    response = client.complete(
+        messages=[
+            SystemMessage("You are a helpful assistant."),
+            UserMessage(search_stream),
+        ],
+        temperature=1.0,
+        top_p=1.0,
+        model=model
+    )
+    output={'client':False,'message':response.choices[0].message.content}
+    print(output)
+    
+    return output
+
+if __name__ == "__main__":
+    app.run()
--- ui/src/App.js
+++ ui/src/App.js
@@ -3,39 +3,102 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import SendIcon from '@mui/icons-material/Send';
-import { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
 function App() {
-  const [messages, setMessages] = useState([])
-  const [inputMessage, setInputMessage] = useState(null)
+    const [messages, setMessages] = useState({ data: [{ message: 'hello', client: true }, { client: false, message: 'As of the most recent estimates in 2023, the population of Bangalore (officially known as Bengaluru), the capital city of Karnataka, India, is approximately **13-14 million** people. The last official Indian Census was conducted in 2011, at which time Bangalore’s population was about 8.5 million. Since then, the city has experienced rapid growth due to migration and urbanization.\n\nFor the most up-to-date and official figures, you may refer to the upcoming results from the next Indian Census when released. However, current reputable estimates generally place Bangalore’s population in the **13–14 million** range.' }, { client: false, message: 'As of the most recent estimates in 2023, the population of Bangalore (officially known as Bengaluru), the capital city of Karnataka, India, is approximately **13-14 million** people. The last official Indian Census was conducted in 2011, at which time Bangalore’s population was about 8.5 million. Since then, the city has experienced rapid growth due to migration and urbanization.\n\nFor the most up-to-date and official figures, you may refer to the upcoming results from the next Indian Census when released. However, current reputable estimates generally place Bangalore’s population in the **13–14 million** range.' }] })
+    const [inputMessage, setInputMessage] = useState({})
+    const containerRef = useRef(null);
+    const [loading, setLoading] = useState(false)
 
-  const handleSend = (e) => {
-    if(inputMessage)setMessages([...messages, inputMessage])
-    console.log(messages)
-    setInputMessage(null)
-  }
+    useEffect(() => {
+        scrollIntoView()
+    }, [messages])
 
-  return (
-    <div className="App">
-      <div className='chat_container'>
-        <Box
-          component="form"
-          className='search_input'
-          sx={{ '& > :not(style)': { color: 'black' } }}
-          noValidate
-          autoComplete="off"
-        >
-          <div className='input_context'>
-            <TextField value={inputMessage?inputMessage:''}onChange={(e)=>setInputMessage(e.target.value)}fullWidth id="standard-basic" label="Standard" variant="standard" />
-            <Button variant="contained" endIcon={<SendIcon />} onClick={handleSend}>
-              Send
-            </Button>
-          </div>
+    const scrollIntoView = () => {
+        const container = document.getElementById('chat_message_container')
+        container.scrollTo({ top: container.scrollHeight, behavior: 'smooth' })
+    }
 
+    const askAI = async (inputSearch) => {
+        setLoading(true)
+        const response = await fetch('https://super-computing-machine-p6grp74g59jf97qx-5000.app.github.dev/query/' + inputSearch, { credentials: 'include' }).then(resp => resp?.json()).then(data => data)
+        setLoading(false)
+        updateLoader(response)
+    }
 
-        </Box>
-      </div>
-    </div>
-  );
+    const updateLoader = (response) => {
+        let tempList = messages?.data;
+        let tempIndex = tempList?.findIndex((a) => a?.loading)
+        tempList[tempIndex] = response
+        setMessages({ data: tempList })
+    }
+
+    const handleSend = (e) => {
+        const loadingMessage = { client: false, loading: true }
+        if (inputMessage?.message) {
+            messages.data = [...messages?.data, inputMessage, loadingMessage]
+            setMessages({ ...messages })
+        }
+        else return
+        setInputMessage({})
+        askAI(inputMessage?.message)
+    }
+
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setInputMessage({ ...inputMessage, message: value, client: true })
+    }
+
+    return (
+        <div className="App">
+            <div className='chat_container'>
+                <div className='chat_message_container' id='chat_message_container' ref={containerRef}>
+                    {messages?.data?.map((messageObj, index) => (
+                        <React.Fragment>
+                            {messageObj?.message &&
+                                <div key={index} className='message_text' style={{ justifyContent: (messageObj?.client ? 'right' : 'left') }}>
+                                    <div className='message_body_inner' style={{ backgroundColor: (messageObj?.client ? '#028251' : '#848484') }}>
+                                        {messageObj?.message}
+                                    </div>
+                                </div>
+                            }
+                            {messageObj?.loading &&
+                                <div key={index} className='message_text' style={{ textAlign: (messageObj?.client ? 'right' : 'left') }}>
+                                    <CircularProgress size={20} />
+                                </div>
+                            }
+                        </React.Fragment>
+
+                    ))}
+                </div>
+
+                <Box
+                    component="form"
+                    className='search_input'
+                    sx={{ '& > :not(style)': { color: 'black' } }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div className='input_context'>
+                        <TextField
+                            value={inputMessage?.message ? inputMessage?.message : ''}
+                            onChange={handleChange}
+                            fullWidth
+                            id="standard-basic"
+                            label="Ask somthing"
+                            variant="standard"
+                        />
+
+                        <Button size='small' loading={loading} variant="contained" endIcon={<SendIcon />} onClick={handleSend}>
+                            Send
+                        </Button>
+                    </div>
+                </Box>
+            </div>
+        </div>
+    );
 }
 
 export default App;
